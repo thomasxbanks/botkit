863eed68a07142074e444049c97deeafac686161
Thom Banks <thom@shoot-the-moon.co.uk>
2016-08-26 15:23:04 +0100

    RC

--- slack_bot.js
+++ slack_bot.js
@@ -63,67 +63,33 @@ This bot demonstrates many of the core features of Botkit:
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-var cleverbot = require("cleverbot.io"),
-    cleverbot = new cleverbot('Q6ycYRPzUZZsC3p9', 'u6L5VD8GolAtwfQwgjMgDz3y7fckyY6Z');
-cleverbot.setNick("Bob");
-cleverbot.create(function(err, session) {
-    if (err) {
-        console.log('cleverbot create fail.');
-    } else {
-        console.log('cleverbot create success.');
-    }
-});
-
 if (!process.env.token) {
     console.log('Error: Specify token in environment');
-    process.exit(1);
+    process.exit(1)
 }
 
-var Botkit = require('./lib/Botkit.js');
-var os = require('os');
+var Botkit = require('./lib/Botkit.js')
+var os = require('os')
 
 var controller = Botkit.slackbot({
     debug: true
-});
+})
 
 var bot = controller.spawn({
     token: process.env.token
-}).startRTM();
+}).startRTM()
 
-
-controller.hears('[cat,kiitos,ketos]', 'direct_message,direct_mention,mention', function(bot, message) {
-    var msg = message.text;
-    var response = [
+controller.hears('',['direct_message','direct_mention','mention'], function(bot, message, reply) {
+    var msg = message.text
+    var responses = [
       '•silent screams•',
       'meep!',
-      'Dreamies?!',
-      'TICKLE STICK!!!!!!'
+      'Dreamies! :open_mouth:',
+      'TICKLE STICK!!!!!!',
+      'nom nom :spider:',
+      'i :heart: :package:'
     ]
-    var rand = Math.floor(Math.random() * response.length) + 0
-    bot.reply(message, response[rand])
-    // cleverbot.ask(msg, function(err, response) {
-    //     if (!err) {
-    //         ;
-    //     } else {
-    //         console.log('cleverbot err: ' + err);
-    //     }
-    // });
+    var rnd = Math.floor(Math.random() * responses.length) + 0
+    var reply = responses[rnd]
+    bot.reply(message, reply)
 })
-
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
-}
